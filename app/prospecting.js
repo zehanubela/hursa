d2e54dfc846cdff163169814f5072b568958cd38
Zeha Irawan <zeha@nubela.co>
2024-09-25 12:10:59 +0800

    details modal

--- app/prospecting.js
+++ app/prospecting.js
@@ -4,19 +4,57 @@ import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PeopleFilter from "@/components/PeopleFilter";
 import CompanyFilter from "@/components/CompanyFilter";
-import { BsPeopleFill, BsBuildingsFill } from "react-icons/bs";
+import { BsPeopleFill, BsBuildingsFill, BsPlus } from "react-icons/bs";
 
 export default function Prospecting() {
+  const [selectedPeople, setSelectedPeople] = useState([]);
+  const [selectedCompanies, setSelectedCompanies] = useState([]);
+
+  const handleExport = () => {
+    console.log(selectedPeople,'selectedPeople')
+    const csvContent = "data:text/csv;charset=utf-8," 
+    + selectedPeople.map(person => 
+        Object.entries(person).flatMap(([key, value]) => 
+          typeof value === 'object' ? Object.values(value).map(String) : String(value)
+        ).join(",")
+      ).join("\n")
+    
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "selected_people.csv");
+    document.body.appendChild(link); // Required for Firefox
+    link.click();
+    document.body.removeChild(link); // Clean up
+  };
 
   return (
-    <div className="p-8 bg-gray-200 h-full">
-      <Tabs defaultValue="people" className="max-w-5xl">
+    <div className="p-8 bg-gray-200">
+      <Tabs defaultValue="people" className="max-w-7xl relative">
         <TabsList>
-          <TabsTrigger value="people" className="flex items-center gap-2"><BsPeopleFill />People</TabsTrigger>
-          <TabsTrigger value="company" className="flex items-center gap-2"><BsBuildingsFill />Companies</TabsTrigger>
+          <TabsTrigger
+            value="people"
+            className="flex items-center gap-2 flex-grow-0"
+          >
+            <BsPeopleFill />
+            People
+          </TabsTrigger>
+          <TabsTrigger
+            value="company"
+            className="flex items-center gap-2 flex-grow-0"
+          >
+            <BsBuildingsFill />
+            Companies
+          </TabsTrigger>
         </TabsList>
-        <TabsContent value="people" className="w-full">
-        <PeopleFilter />
+        <div className="absolute right-0 top-0">
+          <button onClick={handleExport} disabled={selectedCompanies.length === 0 && selectedPeople.length === 0} className="flex items-center gap-2 bg-blue-500 text-white px-4 rounded-md py-2 disabled:opacity-50">
+            Export to CSV
+          </button>
+        </div>
+
+        <TabsContent value="people" className="w-full mt-6">
+          <PeopleFilter selectedPeople={selectedPeople} setSelectedPeople={setSelectedPeople} />
         </TabsContent>
         <TabsContent value="company">
           <CompanyFilter />
--- components/PeopleFilter.jsx
+++ components/PeopleFilter.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import {useState} from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import mockPeopleData from "@/app/person_search_mock.json";
+import { IoLocationSharp } from "react-icons/io5";
+import { FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
+import { Checkbox } from "@/components/ui/checkbox";
+import Modal from "./Modal";
+const PeopleFilter = ({ selectedPeople, setSelectedPeople }) => {
+  // const [people, setPeople] = React.useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [viewDetails, setViewDetails] = useState(null)
 
-const PeopleFilter = () => {
+  // React.useEffect(() => {
+  //   // Fetch data from the JSON file
+  //   fetch("/app/person_search_mock.json")
+  //     .then((response) => response.json())
+  //     .then((data) => setPeople(data.results));
+  // }, []);
+
+  // console.log(mockPeopleData, "mockpeople");
+
+  const handleViewDetails = (person) => {
+    setViewDetails(person)
+    setIsModalOpen(true)
+  }
+
   return (
     <div className="flex gap-6">
       <div className="w-1/4 bg-white rounded-lg p-4">
@@ -26,7 +49,7 @@ const PeopleFilter = () => {
           </AccordionItem>
 
           <AccordionItem value="current-role">
-          <AccordionTrigger>Current Role</AccordionTrigger>
+            <AccordionTrigger>Current Role</AccordionTrigger>
             <AccordionContent>
               <input
                 type="text"
@@ -37,7 +60,7 @@ const PeopleFilter = () => {
           </AccordionItem>
 
           <AccordionItem value="current-company">
-          <AccordionTrigger>Current Company</AccordionTrigger>
+            <AccordionTrigger>Current Company</AccordionTrigger>
             <AccordionContent>
               <input
                 type="text"
@@ -49,10 +72,76 @@ const PeopleFilter = () => {
         </Accordion>
       </div>
       <div className="w-3/4 bg-white rounded-lg p-4 min-h-56">
-        {/* Main content area */}
-        <h3 className="font-bold">Content Area</h3>
-        <div>People Results</div>
+        <div>
+          {mockPeopleData.results.map((person) => (
+            <div
+              key={person.profile.public_identifier}
+              className="flex justify-between border-b-2 border-gray-200 py-2"
+            >
+              <Checkbox checked={selectedPeople.includes(person)} onCheckedChange={(checked) => {
+                if (checked) {
+                  setSelectedPeople([...selectedPeople, person]);
+                } else {
+                  setSelectedPeople(selectedPeople.filter(p => p.profile.public_identifier !== person.profile.public_identifier));
+                }
+              }} />
+              <div className="flex flex-col gap-2 border-r-2 border-gray-200 w-[400px]">
+                <div className="flex items-center gap-2">
+                  <a
+                    className="text-blue-500 block truncate"
+                    href={person.linkedin_profile_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {person.profile.full_name}
+                  </a>
+                  <FaLinkedin />
+                </div>
+                <p className="truncate">
+                  {person.profile.experiences[0].title}
+                </p>
+                <div className="flex items-center gap-2">
+                  <IoLocationSharp />
+                  <span className="truncate">{`${person.profile.city}, ${person.profile.state}, ${person.profile.country}`}</span>
+                </div>
+              </div>
+
+              <div>
+                {person.profile.experiences[0].company_linkedin_profile_url ? (
+                  <Link
+                    href={
+                      person.profile.experiences[0].company_linkedin_profile_url
+                    }
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="text-blue-500 block truncate">
+                      {person.profile.experiences[0].company}
+                    </span>
+                  </Link>
+                ) : (
+                  <span className="block truncate">
+                    {person.profile.experiences[0].company}
+                  </span>
+                )}
+
+                {person.profile.experiences[0].location ? (
+                  <span className="block truncate">
+                    {person.profile.experiences[0].location}
+                  </span>
+                ) : null}
+              </div>
+
+              <div>
+                <button onClick={() => handleViewDetails(person)} className="border-2 border-blue-500 bg-white text-blue-500 px-4 py-2 rounded-md">
+                  View Details
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+      <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen} viewDetails={viewDetails} />
     </div>
   );
 };
