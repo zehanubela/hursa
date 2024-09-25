a5c8f9066d2a4406104fd3f9280e94e4e5345790
Zeha Irawan <zeha@nubela.co>
2024-09-25 12:32:18 +0800

    save api key

--- app/prospecting.js
+++ app/prospecting.js
@@ -1,24 +1,48 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PeopleFilter from "@/components/PeopleFilter";
 import CompanyFilter from "@/components/CompanyFilter";
 import { BsPeopleFill, BsBuildingsFill, BsPlus } from "react-icons/bs";
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function Prospecting() {
   const [selectedPeople, setSelectedPeople] = useState([]);
   const [selectedCompanies, setSelectedCompanies] = useState([]);
+  const [isSettingsOpen, setSettingsOpen] = useState(false);
+  const [apiKey, setApiKey] = useState(null);
+
+  useEffect(() => {
+    const apiKey = localStorage.getItem("apiKey");
+    if (apiKey) {
+      setApiKey(apiKey);
+    }
+  }, []);
+
+  const handleSaveApiKey = () => {
+    localStorage.setItem("apiKey", apiKey);
+    setSettingsOpen(false);
+  };
 
   const handleExport = () => {
-    console.log(selectedPeople,'selectedPeople')
-    const csvContent = "data:text/csv;charset=utf-8," 
-    + selectedPeople.map(person => 
-        Object.entries(person).flatMap(([key, value]) => 
-          typeof value === 'object' ? Object.values(value).map(String) : String(value)
-        ).join(",")
-      ).join("\n")
-    
+    console.log(selectedPeople, "selectedPeople");
+    const csvContent =
+      "data:text/csv;charset=utf-8," +
+      selectedPeople
+        .map((person) =>
+          Object.entries(person)
+            .flatMap(([key, value]) =>
+              typeof value === "object"
+                ? Object.values(value).map(String)
+                : String(value),
+            )
+            .join(","),
+        )
+        .join("\n");
+
     const encodedUri = encodeURI(csvContent);
     const link = document.createElement("a");
     link.setAttribute("href", encodedUri);
@@ -47,19 +71,51 @@ export default function Prospecting() {
             Companies
           </TabsTrigger>
         </TabsList>
-        <div className="absolute right-0 top-0">
-          <button onClick={handleExport} disabled={selectedCompanies.length === 0 && selectedPeople.length === 0} className="flex items-center gap-2 bg-blue-500 text-white px-4 rounded-md py-2 disabled:opacity-50">
+        <div className="absolute right-0 top-0 flex gap-6">
+          <button
+            onClick={() => setSettingsOpen(true)}
+            className="flex items-center gap-2 bg-blue-500 text-white px-4 rounded-md py-2 disabled:opacity-50 font-semibold"
+          >
+            Settings
+          </button>
+          <button className="flex items-center gap-2 bg-white text-blue-500 px-4 rounded-md py-2 disabled:opacity-50 font-semibold">
+            Recent Searches
+          </button>
+          <button
+            disabled={
+              selectedCompanies.length === 0 && selectedPeople.length === 0
+            }
+            onClick={handleExport}
+            className="flex items-center gap-2 bg-blue-500 text-white px-4 rounded-md py-2 disabled:opacity-50 font-semibold"
+          >
             Export to CSV
           </button>
         </div>
 
         <TabsContent value="people" className="w-full mt-6">
-          <PeopleFilter selectedPeople={selectedPeople} setSelectedPeople={setSelectedPeople} />
+          <PeopleFilter
+            selectedPeople={selectedPeople}
+            setSelectedPeople={setSelectedPeople}
+          />
         </TabsContent>
         <TabsContent value="company">
           <CompanyFilter />
         </TabsContent>
       </Tabs>
+
+      <Dialog open={isSettingsOpen} onOpenChange={setSettingsOpen}>
+        <DialogContent>
+          <DialogTitle>Settings</DialogTitle>
+          <h1>Add your API key</h1>
+          <Input
+            type="text"
+            placeholder="API key"
+            value={apiKey}
+            onChange={(e) => setApiKey(e.target.value)}
+          />
+          <Button className="w-min mt-4" onClick={handleSaveApiKey}>Save</Button>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
